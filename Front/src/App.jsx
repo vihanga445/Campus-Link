2b8671182a6e071b71dee4ddac16eb9e701b9841
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2024-12-14 03:00:19 +0530

    added links to each page

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Home from './Pages/Home'; // Import your Home component
+import { BrowserRouter, Route,Routes } from 'react-router-dom'; // Import the Router, Route, and Switch components
+import About from './Pages/About';
+import Signin from './Pages/Signin';
+import Signup from './Pages/Signup';
+import Conatact from './Pages/Contact';
 
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100"> {/* Set a light background color */}
-      {/* Render the Home component */}
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/contact" element={<Conatact />} />
+    
+
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Front/src/Pages/Contact.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Contct() {
+  return (
+    <div>Contact</div>
+  )
+}
