592bdd746a91e4a6890370d77eae22b5ba0459d3
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2024-12-14 12:47:33 +0530

    created the signup page

--- Back/src/controller/auth.controller.js
+++ Back/src/controller/auth.controller.js
@@ -4,6 +4,17 @@ import jwt from "jsonwebtoken";
 import { errorHandler } from "../../utils/error.js";
 export const signup = async (req, res,next) => {
     const {username,email,password} = req.body;
+    if (
+        !username ||
+        !email ||
+        !password ||
+        username === '' ||
+        email === '' ||
+        password === ''
+      ) {
+        next(errorHandler(400, 'All fields are required'));
+      
+    }
     const hashedPassword = bcryptjs.hashSync(password,10); 
 
     const user = new User({username,email,password:hashedPassword});
