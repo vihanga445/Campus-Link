7f3c755c712efbf117b6398e2952efd6edce8feb
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2024-12-14 19:04:29 +0530

    added redux toolkit and redux persist

--- Front/src/Pages/Signin.jsx
+++ Front/src/Pages/Signin.jsx
@@ -1,11 +1,13 @@
 import { Button, Label, TextInput , Alert, Spinner} from 'flowbite-react';
 import { useState } from 'react';
 import { Link , useNavigate} from 'react-router-dom';
+import { useDispatch,useSelector } from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
 
 export default function Signin() {
   const [formData , setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading,error:errorMessage} = useSelector(state=>state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e)=>{
     setFormData({...formData,[e.target.id]:e.target.value.trim()})
@@ -14,12 +16,11 @@ export default function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if ( !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(signInFailure('Please fill out all fields.'));
     }
     try {
      
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/Back/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -27,17 +28,15 @@ export default function Signin() {
       });
       const data = await res.json();
       if(data.success==false){
-        return setErrorMessage(data.message);
-      }
-      setLoading(false);
+dispatch(signInFailure(data.message));      }
       if(res.ok){
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
 
-       setErrorMessage(error.message);
-       setLoading(false);
-
+       dispatch(signInFailure(error.message));
+       
     }
   };
   return (
