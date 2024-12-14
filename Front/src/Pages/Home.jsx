f7b4e06a315f4bb9016d8eaf7d90cc53afc62c0d
MaleeshaFerdinandaz <maleeshaferdinandaz@gmail.com>
2024-12-14 11:27:35 +0530

    Added Announcement Carousel
    cards sliding added

--- Front/src/Pages/Home.jsx
+++ Front/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Carousel from '../components/carousel';
 import UpcomingEventsCarousel from '../components/UpcomingEventsCarousel';
 import ClubsAndSocietiesCarousel from '../components/ClubsAndSocietiesCarousel';
+import AnnouncementsCarousel from '../components/AnnouncementsCarousel';
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
         <Carousel />
         <UpcomingEventsCarousel/>
         <ClubsAndSocietiesCarousel/>
+        <AnnouncementsCarousel/>
+        
       </section>
 
       {/* Footer */}
