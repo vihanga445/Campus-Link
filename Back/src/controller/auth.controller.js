c5e2a7852d17fd5f5d0938fb6f93f5b78becfd0f
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2024-12-14 21:41:59 +0530

    added google OAuth functionalities

--- Back/src/controller/auth.controller.js
+++ Back/src/controller/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../model/user.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { errorHandler } from "../../utils/error.js";
+import e from "express";
 export const signup = async (req, res,next) => {
     const {username,email,password} = req.body;
     if (
@@ -69,3 +70,45 @@ catch(error){
     next(error);
 }
 }
+
+
+export const google = async (req, res, next) => {
+    const { email, name, googlePhotoUrl } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (user) {
+        const token = jwt.sign({ id: user._id }, "secretkey");
+        const { password, ...rest } = user._doc;
+        res
+          .status(200)
+          .cookie('access_token', token, {
+            httpOnly: true,
+          })
+          .json(rest);
+      } else {
+        const generatedPassword =
+          Math.random().toString(36).slice(-8) +
+          Math.random().toString(36).slice(-8);
+        const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+        const newUser = new User({
+          username:
+            name.toLowerCase().split(' ').join('') +
+            Math.random().toString(9).slice(-4),
+          email,
+          password: hashedPassword,
+          profilePicture: googlePhotoUrl,
+        });
+        await newUser.save();
+        const token = jwt.sign({ id: newUser._id }, "secretkey");
+        const { password, ...rest } = newUser._doc;
+        res
+          .status(200)
+          .cookie('access_token', token, {
+            httpOnly: true,
+          })
+          .json(rest);
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
