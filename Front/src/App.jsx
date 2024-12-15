4b2a7815bf6666cdf1645f4c53bc350483cadaf9
MaleeshaFerdinandaz <maleeshaferdinandaz@gmail.com>
2024-12-15 10:56:48 +0530

    Added About Us page
    Done !

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './Pages/Home'; // Import your Home component
 import { BrowserRouter, Route,Routes } from 'react-router-dom'; // Import the Router, Route, and Switch components
-import About from './Pages/About';
+import About from './Pages/AboutUs';
 import Signin from './Pages/Signin';
 import Signup from './Pages/Signup';
 import Conatact from './Pages/Contact';
