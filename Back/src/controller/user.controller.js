17e46f7b29b0b0e07ea7a447b3b12ead209aa23d
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2024-12-17 15:08:15 +0530

    added image upload functionality

--- Back/src/controller/user.controller.js
+++ Back/src/controller/user.controller.js
@@ -1,7 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../../utils/error.js';
 import User from '../model/user.js';
-export const updateUser = async (req, res) => { 
+export const updateUser = async (req, res,next) => { 
     if (req.user.id !== req.params.userId) {
         return next(errorHandler(403, 'You are not allowed to update this user'));
       }
