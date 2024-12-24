e804990e9d129d0b34fdf534d8744d76e8064ae4
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2024-12-24 12:15:22 +0530

    show all posts inside the admin dashboard

--- Back/src/routes/post.route.js
+++ Back/src/routes/post.route.js
@@ -6,6 +6,6 @@ import { create,getposts } from '../controller/post.controller.js';
 const router = express.Router();
 
 router.post('/create',verifyToken,create);
-router.get('/getposts',getposts)
+router.get('/getposts',getposts);
 
 export default router;
