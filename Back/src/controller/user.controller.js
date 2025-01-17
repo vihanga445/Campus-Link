cd8c8a9b63a3323351e682d3b1222d7d9f6ac706
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2025-01-17 20:59:09 +0530

    added signout in header

--- Back/src/controller/user.controller.js
+++ Back/src/controller/user.controller.js
@@ -12,11 +12,7 @@ export const updateUser = async (req, res,next) => {
         req.body.password = bcryptjs.hashSync(req.body.password, 10);
       }
       if (req.body.username) {
-        if (req.body.username.length < 7 || req.body.username.length > 20) {
-          return next(
-            errorHandler(400, 'Username must be between 7 and 20 characters')
-          );
-        }
+     
         if (req.body.username.includes(' ')) {
           return next(errorHandler(400, 'Username cannot contain spaces'));
         }
